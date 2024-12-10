8371113148d940b89c7bf3698e6e93a84ee9b339
Wojciech <writhaler@gmail.com>
2024-12-10 06:18:00 +0100

    Added unfinished solution for day 9 part 2

--- day9/main.py
+++ day9/main.py
@@ -1,6 +1,8 @@
-fileContent = open("./input.txt", "r").read()
+fileContent = open("./input2.txt", "r").read()
 
 nums = list(fileContent.replace("\n", ""))
+
+# --- Solution for part 1 ---
 p1 = 0
 p2 = len(nums) - 1
 disc = []
@@ -9,11 +11,7 @@ while p1 <= p2:
     start = int(nums[p1])
     end = int(nums[p2])
 
-    print(start, p1)
-    print(end, p2)
-
     strToAdd = [p2 // 2]
-    print("strToAdd", strToAdd)
 
     if p1 == 0 or p1 % 2 == 0:
         disc.extend(start * [p1 // 2])
@@ -21,26 +19,69 @@ while p1 <= p2:
 
     else:
         if start > end:
-            print("start > end")
             disc.extend(end * strToAdd)
             nums[p1] = str(start - end)
             p2 -= 2
         elif start < end:
-            print("start < end")
             disc.extend(start * strToAdd)
             nums[p2] = str(end - start)
             p1 += 1
         else:
-            print("start == end")
             disc.extend(start * strToAdd)
             p1 += 1
             p2 -= 2
-
-
-print(disc)
 
 id = 0
 for i, num in enumerate(disc):
     id += i * num
 
-print(id)
+print("Answer to question 1:", id)
+
+# --- Solution for part 2 ---
+nums = list(fileContent.replace("\n", ""))
+queue = []
+
+
+for i, num in enumerate(nums):
+    currentInt = int(nums[i])
+
+    if i == 0 or i % 2 == 0:
+        chunk = "".join(currentInt * str(i // 2))
+        queue.append(chunk)
+    else:
+        chunk = "".join(currentInt * ".")
+        if chunk != "":
+            queue.append(chunk)
+
+
+def findSpace(before, length):
+    for i in range(0, before):
+        if "." in queue[i] and len(queue[i]) >= length:
+            return (i, len(queue[i]) - length)
+
+
+p1 = 0
+p2 = len(queue) - 1
+while p2 > 0:
+    space = findSpace(p2, len(queue[p2]))
+    if space:
+        index = space[0]
+        length = space[1]
+        newNodes = [queue[p2], "." * length]
+        queue[p2] = len(queue[p2]) * "."
+
+        queue.pop(index)
+        if newNodes[1] != "":
+            queue.insert(index, newNodes[1])
+        queue.insert(index, newNodes[0])
+
+    p2 -= 1
+
+
+temp = "".join(queue)
+id = 0
+for i, val in enumerate(list(temp)):
+    if "." not in val:
+        id += i * int(val)
+
+print("Answer to question 2:", id)
